7ffda5a7a7c9ca565a0fb13ba5c0d80382daac50
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-12 20:50:49 +0200

    feat: add store and toggle logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// import react from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import store from './store/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from './cart-slice.js';
+import uiReducer from './ui-slice.js';
+
+const store = configureStore({
+    reducer:{
+        cart: cartReducer,
+        ui: uiReducer
+    }
+})
+
+export default store;
