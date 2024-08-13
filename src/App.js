2002ecedd40059e562da57453b96a409495fb30e
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-13 11:12:41 +0200

    feat: add useEffect to store cart into firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,14 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-redux-01-1a9e3-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  },[cart]);
 
   return (
     <Layout>
